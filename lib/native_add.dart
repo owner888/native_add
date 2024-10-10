7e0a6b413761f3276941737378eccc803005e7d5
kaka <kaka@kaka-M3.local>
2024-10-10 11:16:37 +0700

    删除例子生成的 sum 方法，冲突了，lib_native_add.so 改成 libsum.so

--- lib/native_add.dart
+++ lib/native_add.dart
@@ -6,23 +6,8 @@ import 'dart:isolate';
 
 import 'native_add_bindings_generated.dart';
 
-/// A very short-lived native function.
-///
-/// For very short-lived functions, it is fine to call them on the main isolate.
-/// They will block the Dart execution while running the native function, so
-/// only do this for native functions which are guaranteed to be short-lived.
 int sum(int a, int b) => _bindings.sum(a, b);
 
-/// A longer lived native function, which occupies the thread calling it.
-///
-/// Do not call these kind of native functions in the main isolate. They will
-/// block Dart execution. This will cause dropped frames in Flutter applications.
-/// Instead, call these native functions on a separate isolate.
-///
-/// Modify this to suit your own use case. Example use cases:
-///
-/// 1. Reuse a single isolate for various different kinds of requests.
-/// 2. Use multiple helper isolates for parallel execution.
 Future<int> sumAsync(int a, int b) async {
   final SendPort helperIsolateSendPort = await _helperIsolateSendPort;
   final int requestId = _nextSumRequestId++;
@@ -41,7 +26,7 @@ final DynamicLibrary _dylib = () {
     return DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isAndroid || Platform.isLinux) {
-    return DynamicLibrary.open('lib$_libName.so');
+    return DynamicLibrary.open('libsum.so');
   }
   if (Platform.isWindows) {
     return DynamicLibrary.open('$_libName.dll');
